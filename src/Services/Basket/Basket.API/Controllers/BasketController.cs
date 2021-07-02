ce0cdcef4cb5264b617a80d7e1ab5ebad55c585a
erolsonmez95 <erolsonmez1995@gmail.com>
2021-07-02 22:45:31 +0300

    basket microservice added

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _repository = basketRepository;
+        }
+
+        [HttpGet("{userName}",Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket= await _repository.GetBasket(userName);
+            //if there isn't any basket we should create it for first item available to added the basket.
+            return Ok(basket ?? new ShoppingCart(userName)); 
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket) {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}",Name ="DeleteBasket")]
+        [ProducesResponseType(typeof(void),(int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName) {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+
+
+
+
+
+
+    }
+}
