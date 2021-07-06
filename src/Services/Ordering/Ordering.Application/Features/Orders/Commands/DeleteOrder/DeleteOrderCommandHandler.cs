7a38c10819e4e188b60d1da90ae799c60f94b749
erolsonmez95 <erolsonmez1995@gmail.com>
2021-07-07 02:20:17 +0300

    ordering service with onion architecture

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+        private readonly IOrderRepository _orderRepository;
+
+        public DeleteOrderCommandHandler(IMapper mapper, ILogger<DeleteOrderCommandHandler> logger, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+            _logger.LogInformation("Order deleted successfully.");
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+        private readonly IOrderRepository _orderRepository;
+
+        public UpdateOrderCommandHandler(IMapper mapper, ILogger<UpdateOrderCommandHandler> logger, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+           var orderToUpdate= await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpdate== null)
+            {
+                throw new NotFoundException(nameof(Order),request.Id);
+               
+            }
+
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+            await _orderRepository.UpdateAsync(orderToUpdate);
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+            return Unit.Value;
+        }
+    }
+}
