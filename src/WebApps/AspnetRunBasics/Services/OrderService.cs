2f29f2a61824cdcae44fd28e95ece40cbf91569f
erolsonmez95 <erolsonmez1995@gmail.com>
2021-07-29 00:30:32 +0300

    traditional mvc ui added

--- src/WebApps/AspnetRunBasics/Services/OrderService.cs
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -1,12 +1,11 @@
-﻿using Shopping.Aggregator.Extensions;
-using Shopping.Aggregator.Models;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Shopping.Aggregator.Services
+namespace AspnetRunBasics.Services
 {
     public class OrderService : IOrderService
     {
@@ -19,7 +18,7 @@ namespace Shopping.Aggregator.Services
 
         public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
         {
-            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            var response = await _client.GetAsync($"/Order/{userName}");
             return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
